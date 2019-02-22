2d850a92d155025b82090952e318a956e1d0d511
Csriteja <csriteja.1110@gmail.com>
2019-02-23 00:29:21 +0530

    Enabled Feign

--- src/main/java/com/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyConversionController.java
+++ src/main/java/com/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyConversionController.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,5 +32,17 @@ public class CurrencyConversionController {
         CurrencyConversionBean response = responseEntity.getBody();
 
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from,to);
+        logger.info("{}",response);
+
+        return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
+
     }
 }
